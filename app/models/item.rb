8a91f5e0c2f10b0e83e05160f206f206ae81dd85
grimhamr <rossiadisj@gmail.com>
2022-04-01 14:17:37 +0300

    remove react-rails, fix pellet->pallet typo

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   serialize :status_update_history, Array
   validates :serial_number, uniqueness: true
-  validates :unit, inclusion: { in: %w[Each Pellet Bundle Case] }
+  validates :unit, inclusion: { in: %w[Each Pallet Bundle Case] }
 
   def self.search(search)
     if search
