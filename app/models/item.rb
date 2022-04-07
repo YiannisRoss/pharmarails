ff0f86cb0cbea39b6a561c4a5a279d4c893b03e5
grimhamr <rossiadisj@gmail.com>
2022-04-07 12:18:09 +0300

    remove unused array serialization

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,8 @@
 class Item < ApplicationRecord
-  serialize :status_update_history, Array
   validates :serial_number, uniqueness: true
   validates :unit, inclusion: { in: %w[Each Pallet Bundle Case] }
 
   def self.search(search)
-    if search
-      where('serial_number = ?', search.to_s)
-     
-    end
+    where('serial_number = ?', search.to_s) if search
   end
 end
